ab99680da3579e47a59560638aa89fe7ecfff0bc
Junior Trindade <jrwanderley17@gmail.com>
2025-02-28 15:18:53 -0300

    feat: add new type to model field Type

--- internal/domain/models/cpf_cnpj.go
+++ internal/domain/models/cpf_cnpj.go
@@ -1,24 +1,21 @@
 package models
 
 import (
-	"cpf-cnpj-api/internal/domain/validators"
 	"time"
 )
 
 type CpfCnpj struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Number    string    `json:"number" gorm:"unique"`
-	Type      string    `json:"type"`
-	Blocked   bool      `json:"blocked"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint        `json:"id" gorm:"primaryKey"`
+	Number    string      `json:"number" gorm:"unique"`
+	Type      CpfCnpjType `json:"type"`
+	Blocked   bool        `json:"blocked"`
+	CreatedAt time.Time   `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time   `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
-func (c *CpfCnpj) Validate() bool {
-	if c.Type == "CPF" {
-		return validators.ValidateCPF(c.Number)
-	} else if c.Type == "CNPJ" {
-		return validators.ValidateCNPJ(c.Number)
-	}
-	return false
-}
+type CpfCnpjType string
+
+const (
+	CpfType  CpfCnpjType = "CPF"
+	CnpjType CpfCnpjType = "CNPJ"
+)
