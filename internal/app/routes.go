a555758700e466a25ead0615e633f33a370cc7f7
Junior Trindade <jrwanderley17@gmail.com>
2025-03-03 17:59:28 -0300

    feat: add middleware to check application status

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -4,15 +4,36 @@ import (
 	"cpf-cnpj-api/internal/domain/services"
 	"cpf-cnpj-api/internal/presentation"
 	"cpf-cnpj-api/internal/presentation/middlewares"
+	"sync/atomic"
+	"time"
 
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	startTime          = time.Now()
+	requestCount int64 = 0
 )
 
 func SetupRoutes(authHandler *presentation.AuthHandler, cpfHandler *presentation.CPFHandler, authService *services.AuthService) *gin.Engine {
 	router := gin.Default()
 	router.Use(middlewares.CorsMiddleware())
+	router.Use(middlewares.MetricMiddleware())
 
 	router.GET("/metrics", middlewares.MetricsHandler())
+
+	router.Use(func(c *gin.Context) {
+		atomic.AddInt64(&requestCount, 1)
+		c.Next()
+	})
+
+	router.GET("/status", func(c *gin.Context) {
+		uptime := time.Since(startTime).String()
+		c.JSON(200, gin.H{
+			"uptime":        uptime,
+			"request_count": atomic.LoadInt64(&requestCount),
+		})
+	})
 
 	api := router.Group("/api")
 	api.POST("/register", authHandler.Register)
